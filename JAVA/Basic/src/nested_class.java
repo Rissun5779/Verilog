471e1bcf603e28b5b9000c6992622bdcae5a97ad
rissun5779 <rissun57@gmail.com>
2026-01-17 18:16:51 +0800

    create src, out

---
+++ JAVA/Basic/src/nested_class.java
@@ -0,0 +1,24 @@
+public class nested_class {
+    public int publicA;
+    protected int protectedA;
+    int defaultA;
+    private int privateA;
+
+    public nested_class(){
+        privateA=0;
+    }
+
+    public void printPrivate(){
+        System.out.printf("Private member: %d", privateA);
+    }
+
+    class LocalA{
+        public LocalA(){
+            nested_class B = new nested_class();
+            B.publicA=1;
+            B.privateA=2;
+            B.defaultA=3;
+            B.privateA=4;
+        }
+    }
+}
